961d7bb03817333ce34837adf2f66167f0c77586
Martin Ross <contactmartinross@yahoo.co.uk>
2021-03-13 10:44:49 +0000

    Updated controller.py to include full behaviour tree skeleton.

--- Controller/controller.py
+++ Controller/controller.py
@@ -102,7 +102,7 @@ def create_coaching_tree():
     shot_goal = CreateSubgoal(name="create_shot_goal")
     gen_shot_goal.add_child(shot_goal)
 
-    # Wait for timestep cue (i.e. session goal has been created by guide and we are ready for intro.
+    # Wait for timestep cue (i.e. shot goal has been created by guide and we are ready for intro.
     shot_goal_start = TimestepCue(name="shot_goal_started_timestep")
     shot_goal_start_until = Until(name="shot_goal_start_until", child=shot_goal_start)
     gen_session_goal.add_child(shot_goal_start_until)
@@ -123,8 +123,12 @@ def create_coaching_tree():
     # If pre-instruction is selected run baseline set to make decision of stat choice
     gen_baseline_goal = Sequencer(name="gen_baseline_goal")
     # Create baseline goal
-    basline_goal = CreateSubgoal(name="create_baseline_goal")
-    gen_baseline_goal.add_child(basline_goal)
+    baseline_goal = CreateSubgoal(name="create_baseline_goal")
+    gen_baseline_goal.add_child(baseline_goal)
+    # Wait for timestep cue (i.e. baseline goal has been created by guide and we are ready for intro.
+    baseline_goal_start = TimestepCue(name="baseline_goal_started_timestep")
+    baseline_goal_start_until = Until(name="baseline_goal_start_until", child=baseline_goal_start)
+    gen_baseline_goal.add_child(baseline_goal_start_until)
     # Display baseline pre-instruction behaviour
     baseline_goal_intro = DisplayBehaviour(name="baseline_goal_intro")
     gen_baseline_goal.add_child(baseline_goal_intro)
@@ -148,18 +152,159 @@ def create_coaching_tree():
     shot_goal_intro_output = DisplayBehaviour(name="shot_goal_intro_output")
     shot_goal_intro_sequence.add_child(shot_goal_intro_output)
 
-    shot_goal_repeat = Repeat(name="shot_goal_repeat", child=gen_shot_goal)
-    gen_session_goal.add_child(shot_goal_repeat)
+    #
+    # Conduct coaching for stat
+    #
+    gen_stat_goal = Sequencer(name="gen_stat_goal")
 
-    session_goal_until = Until(name="session_goal_until", child=session_goal+session_goal_selector)
+    # Create stat goal in guide
+    stat_goal = CreateSubgoal(name="create_stat_goal")
+    gen_stat_goal.add_child(stat_goal)
+
+    # Wait for timestep cue (i.e. stat goal has been created by guide and we are ready for intro.
+    stat_goal_start = TimestepCue(name="stat_goal_started_timestep")
+    stat_goal_start_until = Until(name="stat_goal_start_until", child=stat_goal_start)
+    gen_stat_goal.add_child(stat_goal_start_until)
+
+    # Display pre-instruction behaviour
+    stat_goal_intro_output = DisplayBehaviour(name="stat_goal_intro_output")
+    gen_stat_goal.add_child(stat_goal_intro_output)
+
+    # Loop through 4 sets of shot
+    stat_goal_coaching = Sequencer(name="stat_goal_coaching")
+
+    #
+    # Conduct coaching for set
+    #
+    gen_set_goal = Sequencer(name="gen_set_goal")
+
+    # Create set goal in guide
+    set_goal = CreateSubgoal(name="create_set_goal")
+    gen_set_goal.add_child(set_goal)
+
+    # Wait for timestep cue (i.e. set goal has been created by guide and we are ready for intro.
+    set_goal_start = TimestepCue(name="set_goal_started_timestep")
+    set_goal_start_until = Until(name="set_goal_start_until", child=set_goal_start)
+    gen_set_goal.add_child(set_goal_start_until)
+
+    # Display intro set behaviours until pre-instruction
+    set_goal_intro_sequence = Sequencer(name="set_goal_intro_sequence")
+
+    # Get behaviour from policy
+    set_goal_intro_behav = GetBehaviour(name="set_goal_intro_behaviour")
+    set_goal_intro_sequence.add_child(set_goal_intro_behav)
+
+    # Check if pre-instruction
+    set_goal_intro_check_for_pre_instr = CheckForBehaviour(name="set_goal_intro_check_for_pre_instr", behaviour=Policy.A_PREINSTRUCTION)
+    set_goal_intro_pre_instr_negate = Negate(name="set_goal_intro_pre_instr_negate", child=set_goal_intro_check_for_pre_instr)
+    set_goal_intro_sequence.add_child(set_goal_intro_pre_instr_negate)
+
+    # Display selected behaviour if not pre-instruction or questioning
+    set_goal_intro_output = DisplayBehaviour(name="set_goal_intro_output")
+    set_goal_intro_sequence.add_child(set_goal_intro_output)
+
+    set_goal_intro_while = While(name="set_goal_intro_while", child=set_goal_intro_sequence)
+    gen_set_goal.add_chil(set_goal_intro_while)
+
+    # Display pre-instruction behaviour for set
+    set_goal_pre_instr_intro_output = DisplayBehaviour(name="set_goal_pre_instr_intro_output")
+    gen_set_goal.add_child(set_goal_pre_instr_intro_output)
+
+    # Coaching loop fpr set until user initiates end of set event
+    set_goal_coaching_selector = Selector(name="set_goal_coaching_selector")
+
+    # Check for user ending set event
+    set_goal_end_set_event = EndSetEvent(name="set_goal_end_set_event")
+    set_goal_coaching_selector.add_child(set_goal_end_set_event)
+
+    # Individual action coaching loop
+    set_goal_individual_action_sequence = Sequencer(name="set_goal_individual_action_sequencer")
+    # Wait for timestep cue (i.e. user performs action/hits shot)
+    set_goal_individual_action_cue = TimestepCue(name="set_goal_individual_action_cue")
+    set_goal_individual_action_until = Until(name="set_goal_individual_action_until", child=set_goal_individual_action_cue)
+    set_goal_individual_action_sequence.add_child(set_goal_individual_action_until)
+    # Get coaching behaviour from policy for individual action/shot
+    set_goal_individual_action_behav = GetBehaviour(name="set_goal_individual_action_behaviour")
+    set_goal_individual_action_sequence.add_child(set_goal_individual_action_behav)
+    # Display individual action caoching behaviour
+    set_goal_individual_action_output = DisplayBehaviour(name="set_goal_individual_action_output")
+    set_goal_individual_action_sequence.add_child(set_goal_individual_action_output)
+
+    set_goal_individual_action_repeat = Repeat(name="set_goal_individual_action_repeat", child=set_goal_individual_action_sequencer)
+    set_goal_coaching_selector.add_child(set_goal_individual_action_repeat)
+
+    set_goal_coaching_until = Until(name="set_goal_coaching_until", child=set_goal_coaching_selector)
+    gen_set_goal.add_child(set_goal_coaching_until)
+
+    stat_goal_coaching.add_child(gen_set_goal)
+
+    # Set feedback loop until pre-instruction
+    set_goal_feedback_loop = get_feedback_loop(name="set_goal_feedback_loop", behav=Policy.A_PREINSTRUCTION)
+    stat_goal_coaching.add_child(set_goal_feedback_loop)
+
+    stat_goal_coaching_until_count = UntilCount(name="stat_goal_coaching_until_count", max_count=4, child=stat_goal_coaching)
+    gen_stat_goal.add_child(stat_goal_coaching_until_count)
+
+    # Stat feedback loop until pre-instruction
+    stat_goal_feedback_loop = get_feedback_loop(name="stat_goal_feedback_loop", behav=Policy.A_PREINSTRUCTION)
+    gen_stat_goal.add_child(stat_goal_feedback_loop)
+
+    stat_goal_until_count = UntilCount(name="state_goal_until_count", max_count=2, child=gen_stat_goal)
+    gen_shot_goal.add_child(stat_goal_until_count)
+
+    # Shot feedback loop until pre-instruction
+    shot_goal_feedback_loop = get_feedback_loop(name="shot_goal_feedback_loop", behav=Policy.A_PREINSTRUCTION)
+    gen_shot_goal.add_child(shot_goal_feedback_loop)
+
+    shot_goal_repeat = Repeat(name="shot_goal_repeat", child=gen_shot_goal)
+    session_goal_selector.add_child(shot_goal_repeat)
+
+    session_goal_until = Until(name="session_goal_until", child=session_goal_selector)
     gen_session_goal.add_child(session_goal_until)
 
     #
     # Session goal feedback loop
     #
+    session_goal_feedback_loop = get_feedback_loop(name="session_goal_feedback_loop", behav=Policy.A_END)
+    gen_session_goal.add_child(session_goal_feedback_loop)
 
     root.add_child(gen_session_goal)
     return root
+
+def get_feedback_loop(name, behav):
+    # Create feedback loop sequencer
+    sequence_name = name + "_sequence"
+    feedback_loop_sequence = Sequencer(name=sequence_name)
+
+    # Get behaviour from policy
+    feedback_behaviour_name = name + "_behav"
+    feedback_behaviour = GetBehaviour(name=feedback_behaviour_name)
+    feedback_loop_sequence.add_child(feedback_behaviour)
+
+    # Check if given behaviour
+    check_behav_name = name + "_check_for_" + behav
+    feedback_loop_check_for_behav = CheckForBehaviour(name=check_behav_name, behaviour=behav)
+    negate_name = name + "_" + behav + "_check_negate"
+    feedback_loop_behav_check_negate = Negate(name=negate_name)
+    # If pre-instruction, just negate behaviour check
+    if behav == Policy.A_PREINSTRUCTION:
+        feedback_loop_behav_check_negate.add_child(feedback_loop_check_for_behav)
+    else:  # If end, do the check and then display the behaviour
+        feedback_loop_end_sequence = Sequencer(name="feedback_loop_end_sequence")
+        feedback_loop_end_sequence.add_child(feedback_loop_check_for_behav)
+        feedback_loop_display_end_output = DisplayBehaviour(name="feedback_loop_display_end_output")
+        feedback_loop_end_sequence.add_child(feedback_loop_display_end_output)
+        feedback_loop_behav_check_negate.add_child(feedback_loop_end_sequence)
+
+    feedback_loop_sequence.add_child(feedback_loop_behav_check_negate)
+
+    # Display behaviour if not given behaviour
+    output_name = name + "_output"
+    feedback_loop_output = DisplayBehaviour(name=output_name)
+    feedback_loop_sequence.add_child(feedback_loop_output)
+
+    while_name = name + "_while"
+    return While(name=while_name, child=feedback_loop_sequence)
 
 
 if __name__ == '__main__':
