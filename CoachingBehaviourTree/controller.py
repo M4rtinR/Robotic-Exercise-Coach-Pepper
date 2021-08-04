34e97d08d7e204035cc1399608563e49114102ee
Martin Ross <contactmartinross@yahoo.co.uk>
2021-08-04 14:49:42 +0100

    Added P5.1.log

--- CoachingBehaviourTree/controller.py
+++ CoachingBehaviourTree/controller.py
@@ -54,17 +54,17 @@ set_count = 0
 given_score = 0
 
 # Initial values to be changed at the beginning of each session:
-name = "Matthew"
-participantNo = "P4.1"
-ability = 2
-motivation = 8
+name = "Dan"
+participantNo = "P5.1"
+ability = 4
+motivation = 6
 # 1 = DRIVE, 5 = LOB, 0 = DROP
 shot = 1
 # "FH" or "BH"
 hand = "BH"
 # "racketPreparation" = RACKET_PREP, "impactCutAngle" = IMPACT_CUT_ANGLE, "followThroughTime" = FOLLOW_THROUGH_TIME
 stat = "followThroughTime"
-policy = -1
+policy = 3
 leftHand = False
 
 def create_coaching_tree():
