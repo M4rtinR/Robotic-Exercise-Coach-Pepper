91c2c91cc5743c09636a28040a428fc8b4271f65
Martin Ross <contactmartinross@yahoo.co.uk>
2021-08-09 10:13:17 +0100

    Updated ip address

--- CoachingBehaviourTree/controller.py
+++ CoachingBehaviourTree/controller.py
@@ -56,16 +56,16 @@ time_up = False
 time_up_shots = 0
 
 # Initial values
-name = "Alex"
-participantNo = "P6.1"
-ability = 3
-motivation = 7
+name = "Albeer"
+participantNo = "P7.1"
+ability = 6
+motivation = 8
 # 1 = DRIVE, 5 = LOB, 0 = DROP
-shot = 1
+shot = 0
 # "FH" or "BH"
-hand = "BH"
+hand = "FH"
 # "racketPreparation" = RACKET_PREP, "impactCutAngle" = IMPACT_CUT_ANGLE, "followThroughTime" = FOLLOW_THROUGH_TIME
-stat = "followThroughTime"
+stat = "impactCutAngle"
 policy = -1
 
 def create_coaching_tree():
