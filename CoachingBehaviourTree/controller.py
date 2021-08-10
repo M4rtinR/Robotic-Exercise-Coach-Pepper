d59875d9c1e06081df7d2c5de234412057343519
Martin Ross <contactmartinross@yahoo.co.uk>
2021-08-10 19:13:39 +0100

    Added P6.2.log and P6.3.log

--- CoachingBehaviourTree/controller.py
+++ CoachingBehaviourTree/controller.py
@@ -54,17 +54,17 @@ set_count = 0
 given_score = 0
 
 # Initial values to be changed at the beginning of each session:
-name = "Euan"
-participantNo = "P10.2"
-ability = 4
-motivation = 5
+name = "Alex"
+participantNo = "P6.3"
+ability = 3
+motivation = 4
 # 1 = DRIVE, 5 = LOB, 0 = DROP
 shot = 1
 # "FH" or "BH"
-hand = "BH"
+hand = "FH"
 # "racketPreparation" = RACKET_PREP, "impactCutAngle" = IMPACT_CUT_ANGLE, "followThroughTime" = FOLLOW_THROUGH_TIME
-stat = "followThroughTime"
-policy = 10
+stat = "racketPreparation"
+policy = 8
 leftHand = False
 
 def create_coaching_tree():
