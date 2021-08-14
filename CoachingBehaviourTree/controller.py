dfd9e98fb0a5e0e8a57bdaf4d211df3ffc4bb5fe
Martin Ross <contactmartinross@yahoo.co.uk>
2021-08-14 10:44:05 +0100

    Updated initial values

--- CoachingBehaviourTree/controller.py
+++ CoachingBehaviourTree/controller.py
@@ -54,17 +54,17 @@ set_count = 0
 given_score = 0
 
 # Initial values to be changed at the beginning of each session:
-name = "Fraser"
-participantNo = "P17.1"
-ability = 4
-motivation = 8
+name = "Jacques"
+participantNo = "P19.1"
+ability = 5
+motivation = 7
 # 1 = DRIVE, 5 = LOB, 0 = DROP
-shot = 0
+shot = 1
 # "FH" or "BH"
 hand = "FH"
 # "racketPreparation" = RACKET_PREP, "impactCutAngle" = IMPACT_CUT_ANGLE, "followThroughTime" = FOLLOW_THROUGH_TIME
-stat = "impactCutAngle"
-policy = 6
+stat = "racketPreparation"
+policy = -1
 leftHand = False
 
 def create_coaching_tree():
