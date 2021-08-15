da01cf9b563f40789cbd8b0dc3af914fcaddfbcc
Martin Ross <contactmartinross@yahoo.co.uk>
2021-08-15 11:25:33 +0100

    Updated initial values

--- CoachingBehaviourTree/controller.py
+++ CoachingBehaviourTree/controller.py
@@ -56,16 +56,16 @@ time_up = False
 time_up_shots = 0
 
 # Initial values
-name = "Sorcha"
-participantNo = "P21.1"
-ability = 2
+name = "Jacques"
+participantNo = "P19.3"
+ability = 4
 motivation = 7
 # 1 = DRIVE, 5 = LOB, 0 = DROP
-shot = 1
+shot = 0
 # "FH" or "BH"
 hand = "FH"
 # "racketPreparation" = RACKET_PREP, "impactCutAngle" = IMPACT_CUT_ANGLE, "followThroughTime" = FOLLOW_THROUGH_TIME
-stat = "racketPreparation"
+stat = "impactCutAngle"
 policy = -1
 
 def create_coaching_tree():
