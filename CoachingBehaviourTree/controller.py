06a0d2cfeffc2372d49bf743046ab85b65c8dafa
Martin Ross <contactmartinross@yahoo.co.uk>
2021-08-15 13:23:29 +0100

    Added P18.2.log and P18.3.log

--- CoachingBehaviourTree/controller.py
+++ CoachingBehaviourTree/controller.py
@@ -54,18 +54,18 @@ set_count = 0
 given_score = 0
 
 # Initial values to be changed at the beginning of each session:
-name = "Jacques"
-participantNo = "P19.2"
-ability = 4
-motivation = 8
+name = "Heather"
+participantNo = "P18.3"
+ability = 2
+motivation = 10
 # 1 = DRIVE, 5 = LOB, 0 = DROP
 shot = 1
 # "FH" or "BH"
 hand = "BH"
 # "racketPreparation" = RACKET_PREP, "impactCutAngle" = IMPACT_CUT_ANGLE, "followThroughTime" = FOLLOW_THROUGH_TIME
 stat = "followThroughTime"
-policy = 1
-leftHand = True
+policy = 10
+leftHand = False
 
 def create_coaching_tree():
     """
