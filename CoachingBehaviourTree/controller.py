8aab553192b6c5252fe507927ea7fa8d2b83fb21
Martin Ross <contactmartinross@yahoo.co.uk>
2021-08-16 20:37:10 +0100

    Added P20.2.log

--- CoachingBehaviourTree/controller.py
+++ CoachingBehaviourTree/controller.py
@@ -56,16 +56,16 @@ time_up = False
 time_up_shots = 0
 
 # Initial values
-name = "Louise"
-participantNo = "P14.2"
+name = "Mike"
+participantNo = "P20.2"
 ability = 4
 motivation = 7
 # 1 = DRIVE, 5 = LOB, 0 = DROP
-shot = 0
+shot = 1
 # "FH" or "BH"
-hand = "FH"
+hand = "BH"
 # "racketPreparation" = RACKET_PREP, "impactCutAngle" = IMPACT_CUT_ANGLE, "followThroughTime" = FOLLOW_THROUGH_TIME
-stat = "impactCutAngle"
+stat = "followThroughTime"
 policy = -1
 
 def create_coaching_tree():
