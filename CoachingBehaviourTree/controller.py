e6935c29642289f118fbb3106b6f8c15ef6725e3
Martin Ross <contactmartinross@yahoo.co.uk>
2021-08-23 10:30:37 +0100

    Added P23.3.log

--- CoachingBehaviourTree/controller.py
+++ CoachingBehaviourTree/controller.py
@@ -54,17 +54,17 @@ set_count = 0
 given_score = 0
 
 # Initial values to be changed at the beginning of each session:
-name = "Sorcha"
-participantNo = "SensorTest"
-ability = 2
-motivation = 8
+name = "Pietro"
+participantNo = "P23.3"
+ability = 5
+motivation = 7
 # 1 = DRIVE, 5 = LOB, 0 = DROP
-shot = 0
+shot = 1
 # "FH" or "BH"
 hand = "FH"
 # "racketPreparation" = RACKET_PREP, "impactCutAngle" = IMPACT_CUT_ANGLE, "followThroughTime" = FOLLOW_THROUGH_TIME
-stat = "impactCutAngle"
-policy = 0
+stat = "racketPreparation"
+policy = -1
 leftHand = False
 
 def create_coaching_tree():
