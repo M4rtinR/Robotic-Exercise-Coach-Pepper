71c92e291d117cc066179db014e21ed26dd50d59
Martin Ross <contactmartinross@yahoo.co.uk>
2022-11-16 11:50:48 +0000

    Added P7.5

--- CoachingBehaviourTree/config.py
+++ CoachingBehaviourTree/config.py
@@ -220,10 +220,10 @@ double_set_count_feedback = 0
 double_set_count_start = 0
 
 # Initial values to be changed at the beginning of each session:
-name = "Anoopp"
-participantNo = "P8."
+name = "Dan"
+participantNo = "P14."
 participant_filename = participantNo + "_history.txt"
-ability = 6
+ability = 2
 motivation = 8
 # 1 = DRIVE, 5 = LOB, 0 = DROP
 shot = None
