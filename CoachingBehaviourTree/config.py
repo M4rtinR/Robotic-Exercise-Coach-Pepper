33c897d9ccbbcd1aef2d8b940cf7168928629456
Martin Ross <contactmartinross@yahoo.co.uk>
2022-11-17 16:17:41 +0000

    Added P8.6

--- CoachingBehaviourTree/config.py
+++ CoachingBehaviourTree/config.py
@@ -220,10 +220,10 @@ double_set_count_feedback = 0
 double_set_count_start = 0
 
 # Initial values to be changed at the beginning of each session:
-name = "Anoopp"
-participantNo = "P8."
+name = "Bryn"
+participantNo = "P9."
 participant_filename = participantNo + "_history.txt"
-ability = 6
+ability = 2
 motivation = 8
 # 1 = DRIVE, 5 = LOB, 0 = DROP
 shot = None
@@ -232,7 +232,7 @@ hand = None
 # "racketPreparation" = RACKET_PREP, "impactCutAngle" = IMPACT_CUT_ANGLE, "followThroughTime" = FOLLOW_THROUGH_TIME
 stat = None
 policy = 11
-leftHand = False
+leftHand = True
 
 # Values for RL
 alpha = 0.3
