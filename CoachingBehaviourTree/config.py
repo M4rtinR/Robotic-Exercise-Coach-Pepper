83042e563bfb5ba9362da0146c8c383d71003070
Martin Ross <contactmartinross@yahoo.co.uk>
2022-12-02 14:52:42 +0000

    Added P7.10

--- CoachingBehaviourTree/config.py
+++ CoachingBehaviourTree/config.py
@@ -220,8 +220,8 @@ double_set_count_feedback = 0
 double_set_count_start = 0
 
 # Initial values to be changed at the beginning of each session:
-name = "Emma"
-participantNo = "P12."
+name = "James"
+participantNo = "P7."
 participant_filename = participantNo + "_history.txt"
 ability = 2
 motivation = 8
